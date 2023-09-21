5a4bf3e052849caafe41a4c4ca0d48c389a8da3c
Jill Hoffman <93668086+jillhoffman@users.noreply.github.com>
2023-09-21 14:39:57 -0600

    Update Day3_Visualization.py

--- Day3_Visualization.py
+++ Day3_Visualization.py
@@ -1,23 +1,3 @@
-#input.gmt.txt = genes associated to FA
-#string1.txt = all possible connections for all pairs of genes
-
-### more lamen terms instead of code language
-### add checks that make sure both coneced genes are in first file
-
-#PARSE OUT GENES FROM ASSOCIATED LIST
-# with open(input.gmt.txt, 'wr+') as r:
-#     for line in r:
-#         split(line):
-#             for gene:
-#                 FA_gene_list.append(gene)
-#
-#FIND ASSOCIATED GENES IN MASTER LIST
-# for gene in FA_gene_list:
-#     if gene is in string1.txt:
-#WRITE ASSOCIATE GENES IN NEW FILE
-#         write line from string1.txt -> string2.txt
-
-
 associated_gene_file = open("./input.gmt.txt", "r")
 gene_connections = open("./STRING1.txt", "r")
 
@@ -26,9 +6,6 @@ final_connections = []
 
 FA_all_genes = []
 all_connections = []
-
-# def FA_connection_search():
-
 
 for loci in associated_gene_file:
     split_file = loci.split()
@@ -40,30 +17,36 @@ for GCL in gene_connections:
     gene_cons = GCL.split()
     all_connections.append(gene_cons)
 
-def direct_connections(all_connections, FA_gene, FA_all_genes):
+def dup_check(conn, final_connections):
+    dup_check_l = [item for item in final_connections if item[0] == conn[0] and item[1] == conn[1] or item[0] == conn[1] and item[1 == conn[0]]]
+    if len(dup_check_l) == 0:
+        final_connections.append(conn)
+
+def direct_connections(all_connections, FA_gene, FA_all_genes, final_connections):
     direct_FA_gene_connections = [item for item in all_connections if str(item[0]) == str(FA_gene) and item[1] in FA_all_genes]
     if len(direct_FA_gene_connections) == 0:
         print(f"{FA_gene} has no direct connections, checking indirect connections...")
         FA_gene_connections = [item for item in all_connections if str(item[0]) == str(FA_gene)]
         if len(FA_gene_connections) > 0:
-            indirect_connections(FA_gene_connections, all_connections, FA_all_genes)
+            indirect_connections(FA_gene_connections, all_connections, FA_all_genes, final_connections)
 
     else:
         print(f"{FA_gene} has direct connections")
         for conn in direct_FA_gene_connections:
-            final_connections.append(conn)
+            dup_check(conn, final_connections)
 
-def indirect_connections(FA_gene_connections, all_connections, FA_all_genes):
+
+def indirect_connections(FA_gene_connections, all_connections, FA_all_genes, final_connections):
     indirect_genes = [item[1] for item in FA_gene_connections]
     for indir in indirect_genes:
         indirect_gene_connections = [item for item in all_connections if str(item[0]) == str(indir) and str(item[1]) in FA_all_genes]
         if len(indirect_gene_connections) > 1:
             print(f"{FA_gene} has indirect connection")
-            for con in indirect_gene_connections:
-                final_connections.append(con)
+            for conn in indirect_gene_connections:
+                dup_check(conn, final_connections)
 
 for FA_gene in FA_all_genes:
-    direct_connections(all_connections, FA_gene, FA_all_genes)
+    direct_connections(all_connections, FA_gene, FA_all_genes, final_connections)
 
 
 print(final_connections)
@@ -79,7 +62,6 @@ with open("./newInput.txt", 'w') as fp:
         fp.write(f"{r4}\n")
 
 fp.close()
-
 
 
 
